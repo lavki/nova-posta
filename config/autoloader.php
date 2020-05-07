2551061423fe1e3ad215e512744157e1340cfe6a
lavki <sudaryk@gmail.com>
2020-05-07 09:24:46 +0300

    First commit with core structure

---
+++ config/autoloader.php
@@ -0,0 +1,25 @@
+<?php
+
+//$underscores = function ($classname) {
+//    $path = str_replace('\\\\', DIRECTORY_SEPARATOR, $classname);
+//    $path = __DIR__ . "/$path";
+//    if (file_exists("{$path}.php")) {
+//        requare_once("{$path}.php");
+//    }
+//};
+//\spl_autoload_register($underscores);
+
+$namespaces = function ($path) {
+
+    $file_path = str_replace('DoctrineOOP', 'application', dirname(__DIR__) . "\\{$path}.php");
+
+    if (preg_match('/\\\\/',  $file_path)) {
+        $path = str_replace('\\', DIRECTORY_SEPARATOR, $file_path);
+    }
+
+    if (file_exists($path)) {
+        require_once $path;
+    }
+};
+
+spl_autoload_register($namespaces);
